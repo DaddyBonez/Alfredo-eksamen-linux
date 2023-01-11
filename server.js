616068001d03da1d2ef6542faa7a8d4cc3f2b11a
Reign_chad69 <90753568+DaddyBonez@users.noreply.github.com>
2023-01-11 11:38:43 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -17,3 +17,56 @@ app.get("/index.html", (req,res) => { //get request to the root
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
+
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent the form from refreshing the page
+
+    const searchTerm = searchInput.value; // get the search term from the input field
+    console.log(`search shit ${searchInput}`)
+
+    // Perform the search here, for example by filtering an array of data or sending a request to an API
+    // ...
+
+    // Clear the input field for the next search
+    searchInput.value = "";
+});
+
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXRnTVH0dUNg4bNnMcUd3fAk3ltz3bEu0",
+    authDomain: "eksamen-linux-e260c.firebaseapp.com",
+    projectId: "eksamen-linux-e260c",
+    storageBucket: "eksamen-linux-e260c.appspot.com",
+    messagingSenderId: "201684173144",
+    appId: "1:201684173144:web:6a5d700c4c1ddea35aa52d"
+  };
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'Eier')
+
+// get collection data
+getDocs(colRef)
+  .then(snapshot => {
+    // console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach(doc => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(Eier)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
